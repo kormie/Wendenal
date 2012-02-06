d2451547bf17d9857eb075bf5e6fa10f780aa542
David Kormushoff <kormie@gmail.com>
2012-02-06 00:06:32 -0600

    Move repeated functions into private methods

--- lib/wendenal.rb
+++ lib/wendenal.rb
@@ -1,6 +1,5 @@
 #!/usr/bin/ruby
 
-
 class Wendenal
 
   untaxed = ["book", "chocolate", "headache pills"]
@@ -31,17 +30,12 @@ class Wendenal
       tax << item[2]
     end
 
-    tax = tax.inject(:+)
-    @taxed_total = tax
+    @taxed_total = tax.inject(:+)
+    @total_tax = @taxed_total - @original_total
   end
 
-  def taxed_cost(original_price, tax_percentage)
-    unrounded_tax = (original_price * tax_percentage) / 100
-    rounded_tax = (unrounded_tax * 20).ceil / 20.0
-    total_item_cost = original_price + rounded_tax
-  end
 
-  def calculate_total
+  def calculate_original_total
 
     total = []
 
@@ -50,25 +44,35 @@ class Wendenal
       total << item[2]
     end
 
-    @total =  total.inject(:+)
+    @original_total =  total.inject(:+)
 
   end
 
   def print_receipt
     @items.each do |item|
-      puts "#{item[0]} #{item[1]}: #{"%.2f" % item[2]}"
+      puts "#{item[0]} #{item[1]}: #{formated_price(item[2])}"
     end
 
-    puts "Sales Tax: #{"%.2f" % (@taxed_total - @total)}"
-    puts "Total: #{"%.2f" % @taxed_total}"
+    puts "Sales Tax: #{formated_price(@total_tax)}"
+    puts "Total: #{formated_price(@taxed_total)}"
   end
 
+private
 
+  def taxed_cost(original_price, tax_percentage)
+    unrounded_tax = (original_price * tax_percentage) / 100
+    rounded_tax = (unrounded_tax * 20).ceil / 20.0
+    total_item_cost = original_price + rounded_tax
+  end
+
+  def formated_price(unformated_price)
+    "%.2f" % unformated_price
+  end
 
 end
 
 transaction = Wendenal.new
 transaction.parse_items
-transaction.calculate_total
+transaction.calculate_original_total
 transaction.calculate_tax
 transaction.print_receipt
